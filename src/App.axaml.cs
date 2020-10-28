455a4728463fd4064804c7f390c90eb433271821
Victor Milovanov <lost@losttech.software>
2020-10-28 14:32:28 -0700

    repro https://github.com/AvaloniaUI/Avalonia/issues/4965

---
+++ src/App.axaml.cs
@@ -0,0 +1,21 @@
+﻿namespace DetectUI {
+    using System;
+    using Avalonia;
+    using Avalonia.Controls.ApplicationLifetimes;
+    using Avalonia.Markup.Xaml;
+    public class App : Application {
+        public override void Initialize() {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted() {
+            if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                desktop.MainWindow = new MainWindow();
+            } else {
+                // throw new PlatformNotSupportedException();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ src/MainWindow.axaml.cs
@@ -0,0 +1,26 @@
+﻿namespace DetectUI {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Avalonia;
+    using Avalonia.Controls;
+    using Avalonia.Interactivity;
+    using Avalonia.Markup.Xaml;
+
+    public class MainWindow : Window {
+        public MainWindow() {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            //this.image = this.Get<Image>("CodeImage");
+            //this.openFileButton = this.Get<Button>("OpenFileButton");
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
